cfa88f5d1201e443a7f91e98f30bcd8e3a29b841
Ambady <ambadydileep69@gmail.com>
2025-06-18 21:24:12 +0530

    doubly linkedlist

---
+++ problems/easy/doubly_linkedlist.py
@@ -0,0 +1,35 @@
+class Node:
+    def __init__(self,data):
+        self.data = data
+        self.next = None
+        self.prev = None
+        
+class DoublyLinkedList:
+    def __init__(self):
+        self.head = None
+        
+    def append(self,data):
+        new_node = Node(data)
+        
+        if self.head is None:
+            self.head = new_node
+            return
+        
+        current = self.head
+        while current.next:
+            current = current.next
+        current.next = new_node
+        new_node.prev = current
+        
+    def print_forward(self):
+        current = self.head
+        while current:
+            print(current.data, end=" <--> ")
+            current = current.next
+        print("None")
+            
+dll = DoublyLinkedList()
+dll.append(10)
+dll.append(20)
+dll.append(30)
+dll.print_forward()
