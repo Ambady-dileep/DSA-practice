cd010ff631fb533f14d1713522e26e9baf9a46ed
Ambady <ambadydileep69@gmail.com>
2025-06-22 23:02:00 +0530

    doubly linkedlist in pys

--- problems/easy/doubly_linkedlist.py
+++ problems/easy/doubly_linkedlist.py
@@ -149,25 +149,51 @@
 class Node:
     def __init__(self,data):
         self.data = data
-        self.prev = None
         self.next = None
+        self.prev = None
         
-        
-class LinkedList:
+class DoublyLinkedList:
     def __init__(self):
         self.head = None
-
+        
     def append(self,data):
         new_node = Node(data)
         if self.head is None:
             self.head = new_node
             return
-
-        # Case 2: Traverse to the last node
+        
         current = self.head
         while current.next:
             current = current.next
-            
         current.next = new_node
         new_node.prev = current
         
+    def print_backward(self):
+        current = self.head
+        if current is None:
+            print("None (empty list)")
+            return
+        
+        while current.next:
+            current = current.next
+            
+        while current:
+            print(current.data, end=" <-> ")
+            current = current.prev
+        print("None")
+        
+    def print_forward(self):
+        current = self.head
+        while current:
+            print(current.data, end=" <-> ")
+            current = current.next
+        print("None")
+
+        
+dll = DoublyLinkedList()
+dll.append(10)
+dll.append(20)
+dll.append(30)
+dll.append(40)
+dll.print_forward()
+dll.print_backward()
