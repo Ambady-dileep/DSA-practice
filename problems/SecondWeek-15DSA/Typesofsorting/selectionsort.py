692e45f507da0472fc613f778a51c651b5cbd2c1
Ambady <ambadydileep69@gmail.com>
2025-06-28 16:18:39 +0530

    bubble sort technique

---
+++ problems/SecondWeek-15DSA/Typesofsorting/selectionsort.py
@@ -0,0 +1,12 @@
+def selection_sort(arr):
+    n = len(arr)
+    for i in range(n):
+        min_index = i
+        for j in range(i+n,n):
+            if arr[j]<arr[min_index]:
+                min_index = j
+        arr[i],arr[min_index]= arr[min_index],arr[i]
+    return arr 
+
+arr = [4,2,7,1]
+print(selection_sort(arr))
