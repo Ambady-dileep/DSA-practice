e8e0090e57fd9095fe847f460fa8b311ccc8b948
Ambady <ambadydileep69@gmail.com>
2025-07-19 20:01:05 +0530

    Theory concepts of the 16th week

---
+++ problems/ThirdWeek-16DSA/Graph.py
@@ -0,0 +1,20 @@
+# 🔑 What is a Graph?
+# A Graph is a collection of:
+
+# Nodes (aka vertices) 👉 points
+
+# Edges 👉 connections between those points
+
+# Example: Think of Facebook. Each person = node. Each friendship = edge.
+
+# ✅ Graph Terminology (in Plain English)
+# Term	             Meaning	               Example
+# Node(Vertex)       A point	             A, B, C, D
+# Edge	             A connection	         A → B
+# Undirected	     Edge without direction  A-B is same as B-A
+# Directed (Digraph) Edge has direction	     A → B is not B → A
+# Weighted	         Edge has cost/value	 A -3-> B (weight = 3)
+# Unweighted	     No value on edge	     A → B (just a link)
+# Degree	         No. of edges on a node	 A has 2 edges → degree = 2
+
+
