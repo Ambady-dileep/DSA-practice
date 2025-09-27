c1e67ef91cdf46473b42cfb4c47830e7c7c2bca4
Ambady Dileep <ambadydileep.pro@gmail.com>
2025-09-27 23:17:08 +0530

    Generators in python'

--- problems/python_practicals/generators.py
+++ problems/python_practicals/generators.py
@@ -1,12 +1,25 @@
-def func(a):
-    for i in a:
-        yield i
+# def func(a):
+#     for i in a:
+#         yield i
 
-a = [1,2,3,4,5]
+# a = [1,2,3,4,5]
 
-gen = func(a)
+# gen = func(a)
 
-print(next(gen))
-print(next(gen))
-print(next(gen))
-print(next(gen))
+# print(next(gen))
+# print(next(gen))
+# print(next(gen))
+# print(next(gen))
+
+
+# def func(b):
+#     for i in b:
+#         yield i
+
+# b = [1,2,3,4,5]
+# gen = func(b)
+
+# print(next(gen))
+# print(next(gen))
+# print(next(gen))
+# print(next(gen))
